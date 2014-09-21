f3f4c97426baa114c0c0c3f77aca690f0aec3845
Karol Stępniewski <karol.stepniewski@gmail.com>
2014-09-21 15:23:48 +0200

    removed unneeded files

--- cs-server/db/change.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type Change struct {
-	Id        int64 `db:"id"`
-	FileId    int64 `db:"file_id"`
-	CursorOld int64 `db:"cursor_old"`
-	CursorNew int64 `db:"cursor_new"`
-	UserId    int64 `db:"user_id"`
-}
--- cs-server/protocols/common.go
+++
@@ -1,8 +0,0 @@
-package protocols
-
-import "github.com/Sirupsen/logrus"
-
-type  Protocol interface {
-	RunProtocol(address string, port int) error
-	SetLogger(logger *logrus.Logger)
-}
